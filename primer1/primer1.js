fd27a0b7e26c30cd9ef35cf944f996e4f9ab9709
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2025-02-04 14:13:48 +0000

    rolled some changes back

--- primer1/primer1.js
+++ primer1/primer1.js
@@ -1,31 +1,31 @@
 function celsiusToFahrenheit(celsius) {
-    return `The temperature converted from Celsius in Fahrenheit is: ${parseFloat(celsius) * 1.8 + 32}`;
+    return parseFloat(celsius) * 1.8 + 32;
 }
 
 function fahrenheitToCelsius(fahrenheit) {
     let convertedToCelsus = (fahrenheit - 32) / 1.8;
     console.log(typeof(Number(convertedToCelsus)));
-    return `The temperature converted from Fahrenheit in Celsius is: ${Number((convertedToCelsus).toFixed(2))}`;
+    return Number((convertedToCelsus).toFixed(2));
 }
 
 function celsiusToKelvin(celsius) {
     let tempInKelvin = celsius + 273.15;
-    return `The temperature converted from Celsius in Kelvin is: ${Number(tempInKelvin)}`;
+    return Number(tempInKelvin);
 }
 
 function fahrenheitToKelvin(fahrenheit) {
     let tempInKelvin = fahrenheitToCelsius(fahrenheit) + 273.15;
-    return `The temperature converted from Farenheit in Kelvin is: ${tempInKelvin}`;
+    return tempInKelvin;
 }
 
 function kelvinToCelsius(kelvin) {
     let tempInCelsius = kelvin - 273.15;
-    return `The temperature converted from Kelvin in Celsius is:${Number(tempInCelsius)}`;
+    return Number(tempInCelsius);
 }
 
 function kelvinToFahrenheit(kelvin) {
     let tempInFahrenheit = (kelvin - 273.15) * 1.8 + 32;
-    return `The temperature converted from Kelvin in Fahrenheit is: ${Number(tempInFahrenheit)}`;
+    return Number(tempInFahrenheit);
 }
 
 
